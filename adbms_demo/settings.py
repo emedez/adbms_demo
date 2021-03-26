a5d1a787f18ae72dd16b881897c13ec0da7c4f2b
Elmer Medez <emedez@gmail.com>
2021-03-27 05:29:15 +0800

    Use PostgreSQL database

--- adbms_demo/settings.py
+++ adbms_demo/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'adbms_demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': 'password123',
+        'NAME': 'postgres',
+        'PORT': 5432
     }
 }
 
@@ -105,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Manila'
 
 USE_I18N = True
 
